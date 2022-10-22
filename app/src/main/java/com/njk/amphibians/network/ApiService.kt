16fc9c0fa3bf95141d0e57858fffde7d4b058eb3
nijuyonkadesu <24ksriramv@gmail.com>
2022-10-22 16:43:51 +0530

    Finish networks

--- app/src/main/java/com/njk/amphibians/network/ApiService.kt
+++ app/src/main/java/com/njk/amphibians/network/ApiService.kt
@@ -4,17 +4,31 @@ import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
+import retrofit2.http.GET
 
+// 1. Base URL
 private val BASE_URl = "https://developer.android.com/courses/pathways/android-basics-kotlin-unit-4-pathway-2/"
 
-// moshi instance using KotlinJsonAdapter
+// 2. moshi instance using KotlinJsonAdapter
 private val moshi = Moshi.Builder()
     .add(KotlinJsonAdapterFactory())
     .build()
 
-// Retrofit instance using moshi
+// 3. Retrofit instance using moshi
 private val retrofit = Retrofit.Builder()
     .addConverterFactory(MoshiConverterFactory.create(moshi))
     .baseUrl(BASE_URl)
     .build()
 
+// 4. Interface on how to communicate with API service
+interface ApiService {
+    @GET("amphibians")
+    suspend fun getAmphibian(): List<Amphibian>
+}
+
+// 5. Singleton object to access API service
+object AmphibianApi {
+    val retrofitService: ApiService by lazy {
+        retrofit.create(ApiService::class.java)
+    }
+}
